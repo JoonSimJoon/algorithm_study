efb633ec2cad0ae0e4fc7e152ace08e19d0f89f0
SimJoonTTMD <wesley0940@gmail.com>
2021-02-14 20:15:00 +0900

    20210214

--- algorithm/17000/17250.cpp
+++ algorithm/17000/17250.cpp
@@ -9,23 +9,20 @@ struct s {
 };
 s arr[100010];
 ll find(ll x) {
-	// 루트 노드는 부모 노드 번호로 자기 자신을 가진다.
 	if (arr[x].key == x) {
 		return x;
 	}
 	else {
-		// 각 노드의 부모 노드를 찾아 올라간다.
 		return find(arr[x].key);
 	}
 }
 void unit(ll x, ll y) {
 	x = find(x);
 	y = find(y);
-	ll tmp = arr[x].cnt + arr[y].cnt;
 	arr[y].key = x;
-	arr[x].cnt = tmp;
-	arr[y].cnt = tmp;
+	arr[x].cnt += arr[y].cnt;
 }
+
 int main() {
 	scanf("%lld %lld", &N, &M);
 	for (ll i = 0; i < N; i++) {
@@ -37,8 +34,12 @@ int main() {
 		scanf("%lld %lld", &a, &b);
 		a--;
 		b--;
-		unit(a, b);
-		printf("%lld %lld\n", arr[a].cnt, arr[b].cnt);
+		ll x = find(a);
+		ll y = find(b);
+		if (x != y) {
+			unit(a, b);
+		}
+		printf("%lld \n", arr[x].cnt);
 	}
 	return 0;
 }
