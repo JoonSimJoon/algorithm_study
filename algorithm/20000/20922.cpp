206741973d11f57980d860db0c61e7ccc5414ae9
SimJoonTTMD <wesley0940@gmail.com>
2021-03-01 19:04:05 +0900

    20210301

--- algorithm/20000/20922.cpp
+++ algorithm/20000/20922.cpp
@@ -2,26 +2,45 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+#define ll long long
+ll N, K, st, en, len = -1;
+ll arr[200010], cnt[100011];
 
-struct s {
-	int key, val;
-};
-int N, K, a, ans = -1;
-vector<int> v;
-s arr[100010];
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie();
 	cout.tie();
 	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
-		cin >> a;
-		v.push_back(a);
-		arr[a].val++;
-		if (arr[a].val == K) {
-			
+	for (ll i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	cnt[arr[st]]++;
+	en++;
+	cnt[arr[en]]++;
+	while (1) {					
+		if (st >= N) break;
+		else{	
+			//cout << st << " " << en << "\n";
+			if (cnt[arr[en]] > K) {
+				//cout << "*" << st << " " << en << "\n";
+				while (1) {
+					if (cnt[arr[en]] <= K) break;
+					cnt[arr[st]]--;
+					st++;
+				}
+			}
+			else {
+				if (len < en - st + 1) {
+					len = en - st + 1;
+				}
+				if (en < N - 1) {
+					en++;
+					cnt[arr[en]]++;
+				}
+				else st++;
+			}
 		}
 	}
-
+	cout << len << "\n";
 	return 0;
 }
