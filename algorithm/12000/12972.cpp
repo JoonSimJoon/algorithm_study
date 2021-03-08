0c136877b37362e3b036ebc6d746ab8ebb4dc6ff
SimJoonTTMD <wesley0940@gmail.com>
2021-03-08 23:58:25 +0900

    20210308

--- algorithm/12000/12972.cpp
+++ algorithm/12000/12972.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int N, a;
-vector<int> v;
+vector<int> v,ans;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie();
@@ -14,6 +14,26 @@ int main() {
 		cin >> a;
 		v.push_back(a);
 	}
-
+	if (N == 1) {
+		cout << v[0] << "\n";
+	}
+	else {
+		sort(v.begin(), v.end());
+		reverse(v.begin(), v.end());
+		int cp = v[0];
+		int cnt = 1;
+		for (int i = 1; i < N * N; i++) {
+			if (v[i] == cp) cnt++;
+			else {
+				if (cnt % 2 == 1) {
+					ans.push_back(cp);
+					if (ans.size() == N) break;
+				}
+				cp = v[i];
+				cnt = 1;
+			}
+		}
+		for (int i = 0; i < N; i++) cout << ans[i] << " ";
+	}
 	return 0;
 }
