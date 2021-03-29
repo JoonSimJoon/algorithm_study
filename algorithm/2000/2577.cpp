16be74656aa4dcb7d59f7e59bd821f268117f7fc
SimJoon <wesley0940@gmail.com>
2021-03-29 23:02:31 +0900

    20210329

---
+++ algorithm/2000/2577.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long
+
+int arr[11];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll A, B, C;
+	cin >> A >> B >> C;
+	ll ans = A * B * C;
+	while (ans > 0) {
+		arr[ans % 10]++;
+		ans /= 10;
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << "\n";
+	}
+	return 0;
+}
