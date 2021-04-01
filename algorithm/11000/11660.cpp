95aa6df86d3d8dd51117ef93e34bb0c5b842554e
SimJoon <wesley0940@gmail.com>
2021-04-02 00:29:32 +0900

    20210402

--- algorithm/11000/11660.cpp
+++ algorithm/11000/11660.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 using namespace std;
-
-int N, M;
-int arr[1050][1050];
-int dp[1050][1050];
+#define ll long long
+ll N, M;
+ll arr[1050][1050];
+ll dp[1050][1050];
+ll ax, ay, bx, by;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
+	for (ll i = 1; i <= N; i++) {
+		for (ll j = 1; j <= N; j++) {
 			cin >> arr[i][j];
 			dp[i][j] = arr[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
 		}
 	}
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
+	/*for (ll i = 1; i <= N; i++) {
+		for (ll j = 1; j <= N; j++) {
 			cout << dp[i][j] << " ";
 		}
 		cout << "\n";
+	}*/
+	for (ll i = 0; i < M; i++) {
+		cin >> ax >> ay >> bx >> by;
+		ll ans = dp[bx][by] - dp[ax-1][by] - dp[bx][ay-1] + dp[ax-1][ay-1];
+		
+		cout << ans << "\n";
 	}
 	return 0;
 }
