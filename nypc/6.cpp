2e65b1c122eea421375f3033e4e946dd3376da84
SimJoon <wesley0940@gmail.com>
2021-08-25 00:18:00 +0900

    20210825

--- nypc/6.cpp
+++ nypc/6.cpp
@@ -1,12 +1,13 @@
-    #include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 #define ll long long
-
+priority_queue <pair <ll,ll> > pq;
 ll N,K,cnt;
 vector<ll> v;
-
+ll chk[300010];
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -15,13 +16,30 @@ int main(){
 	for(ll a,i=0;i<N;i++){
 		cin>>a;
 		v.push_back(a);
+		pq.push({-a,i});
 	}
-	ll st=0;
-	cnt=1;i
-	for(ll i=0;i<N;i++){
-		if(v[i]>v[st]+K || v[st]>v[i]){
-			st=i;
-			cnt++;
+	while(!pq.empty()){
+		ll i;
+		int now=pq.top().second;
+		pq.pop();
+		if(chk[now]==1) continue;
+		chk[now]=1;
+		cnt++;
+		i = now-1;
+		while(i>=0){
+			if(v[now]+K<v[i] || v[now]>v[i]) break;
+			else{
+				chk[i]=1;
+				i--;
+			}
+		}
+		i=now+1;
+		while(i<N){
+			if(v[now]+K<v[i] || v[now]>v[i]) break;
+			else{
+				chk[i]=1;
+				i++;
+			}
 		}
 	}
 	cout<<cnt;
