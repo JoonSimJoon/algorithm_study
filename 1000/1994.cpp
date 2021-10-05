481332f566c006d354410adc231295670898cc97
SimJoon <wesley0940@gmail.com>
2021-10-06 00:01:07 +0900

    20211006

--- 1000/1994.cpp
+++ 1000/1994.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 #define ll long long 
-
+ll N;
+vector<ll> v;
 int main(){
 	ios::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
-
+	cin>>N;
+	for(ll a,i=0;i<N;i++){
+		cin>>a;
+		v.push_back(a);
+	}
+	sort(v.begin(),v.end());
+	
 	return 0;
 }
